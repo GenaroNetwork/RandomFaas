9cc4b73e4f52b5daa109573297b31dbae4f8f343
lizhengxiang <lizhengxiang@genaro.network>
2019-01-24 21:15:01 +0800

    solidity do update a lot, needs to build first

--- RandomFaas.sol
+++ RandomFaas.sol
@@ -1,13 +1,14 @@
-pragma solidity ^0.4.13;
+pragma solidity>=0.4.20;
 
 /**
  * The random faas provide the interface that smooths the random function 
  * and make it simpler for users to call 
  * Author : waylon(waylon@genaro.network)
  */
+
 contract RandomFaas {
 
-	address public owner;
+	address payable public owner;
 
 	uint public counts;
 	uint public limit;
@@ -37,10 +38,10 @@ contract RandomFaas {
 	event NeedNewGroup(uint counts);
 	event randomOutput(address group, uint randomNumber);
 
-	function RandomFaas (address _owner, uint _countdown, uint _limit) 
+    constructor (address payable _owner, uint _countdown, uint _limit) public 
 	{
 		require(_owner != address(0));
-		require(_counttime != 0);
+		require(_countdown != 0);
 		require(_limit != 0);
 
 		owner = _owner;
@@ -49,7 +50,7 @@ contract RandomFaas {
 	}
 	
 	//deposit
-	function () public payable {
+	function () external payable {
 		deposit();
 	}
 
@@ -61,7 +62,7 @@ contract RandomFaas {
 	function withdraw () public 
 	only(owner)
 	{
-		require(owner.send(this.balance));	
+		require(owner.send(address(this).balance));	
 	}
 	
 	
@@ -76,49 +77,96 @@ contract RandomFaas {
 	
 	function stakeJoin () public payable
 	{
-		require(_operator != address(0));
 		require(GroupList[counts].length<limit);
-		NeedNewGroup(counts);
+		emit NeedNewGroup(counts);
 		
-		updateValue(RandomGroup[counts][msg.sender],0,0,msg.value);
+		updateValue(RandomGroup[counts][msg.sender],0,bytes32(0),msg.value);
 		GroupList[counts].push(msg.sender);
 
 	}
 	
-	function updateValue (Req[] storage group, uint _randomNum, uint _signature) 
+	function updateValue (Req[] storage group, uint _randomNum, bytes32 _signature,uint _value) 
 	internal
 	{
 		uint _ptr = group.length;
 
 		Req storage newMember = group[group.length++];
-		newMember.randomNum = _randomNum;
-		newMember.signature = _signature;
+        Req storage oldMember = group[_ptr];
+	    
+		newMember.randomNum = oldMember.randomNum>_randomNum?oldMember.randomNum:_randomNum;
 
+        if(_signature[0] != 0){
+			newMember.signature = _signature;
+	    }else{
+			newMember.signature = oldMember.signature;
+		}
+
+
+	    newMember.value = oldMember.value> _value?oldMember.value:_value;
 
 	}
 	
 	function commitRandom (uint _randomNum, uint _count) public
 	{
-
+		require(_count<counts);
 		require(_randomNum != 0);
-		RandomGroup[_count][msg.sender].randomNum = _randomNum;
+        
+	    updateValue(RandomGroup[_count][msg.sender],_randomNum,bytes32(0),0);
 
 	}
 
-	function commitSignRandom (bytes32 _sign, uint _count) public
-	{
-		require(RandomGroup[_count][msg.sender].signature == bytes32(0));
-		require(sha3(RandomGroup[_count][msg.sender].randomNum,msg.sender) == _sign);
-		RandomGroup[_count][msg.sender].signature = _sign;
+	function commitSignRandom (bytes32 _message, bytes memory _signature, uint _count) public
+	{  
+		Req storage latestInfo = RandomGroup[_count][msg.sender][RandomGroup[_count][msg.sender].length];
+		require(latestInfo.signature[0] == 0);
+        require(ecrecovery(_message,_signature)==msg.sender);
+	
+ 	    updateValue(RandomGroup[_count][msg.sender],0,_message,0);	
 		GroupOption[_count].NoQuitFlag = true;
 		GroupOption[_count].CloseTime = block.number+countdown;
 
 	}
 	
- 	function closeProcess (uint _count) returns (bytes32 _result) public
+//https://blog.csdn.net/code_segment/article/details/79952722
+//for the signature recovery function with minor changes
+
+function ecrecovery(bytes32 hash, bytes memory sig) public returns (address) {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+
+    if (sig.length != 65) {
+      return address(0);
+    }
+
+    assembly {
+      r := mload(add(sig, 32))
+      s := mload(add(sig, 64))
+      v := and(mload(add(sig, 65)), 255)
+    }
+
+    // https://github.com/ethereum/go-ethereum/issues/2053
+    if (v < 27) {
+      v += 27;
+    }
+
+    if (v != 27 && v != 28) {
+      return address(0);
+    }
+
+    /* prefix might be needed for geth only
+     * https://github.com/ethereum/go-ethereum/issues/3731
+     */
+    // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+    // hash = sha3(prefix, hash);
+
+    return ecrecover(hash, v, r, s);
+  }
+
+ 	function closeProcess (uint _count) public  returns (bytes32 _result)
  	{
-		require(RandomGroup[_count][msg.sender].randomNum != 0);
-		require(RandomGroup[_count][msg.sender].signature != bytes32(0));
+		require(RandomGroup[_count][msg.sender][RandomGroup[_count][msg.sender].length].randomNum != 0);
+		require(RandomGroup[_count][msg.sender][RandomGroup[_count][msg.sender].length].signature[0] != 0);
 		
 		address _addr;
 		//to do: in certain time should not do closeProcess
@@ -127,7 +175,7 @@ contract RandomFaas {
 			//calculate the random result
 			for (uint cnt = 0; cnt < GroupList[_count].length; cnt++){
 				_addr = GroupList[_count][cnt];
-				_result = _result ^ RandomGroup[_count][_addr];
+				_result = _result ^ RandomGroup[_count][_addr][RandomGroup[_count][msg.sender].length].signature;
 			}			
 		}
 
@@ -138,15 +186,17 @@ contract RandomFaas {
 		
 		uint _punish;
 		address _addr;
-		address[] _bounter;
 		uint _payback;
+		
+		address[10] memory _bounter;
+		uint counter;
 
 		for(uint cnt=0; cnt< GroupList[_count].length;cnt++){
-			_addr = RandomGroup[_count][cnt];
-			if(RandomGroup[_count][_addr].randomNum == 0 || RandomGroup[_count][_addr].signature = bytes32(0)){
-				_punish += RandomGroup[_count][_addr].value;
+			_addr = GroupList[_count][cnt];
+			if(RandomGroup[_count][_addr][RandomGroup[_count][msg.sender].length].randomNum == 0 || RandomGroup[_count][_addr][RandomGroup[_count][msg.sender].length].signature[0] == 0){
+				_punish += RandomGroup[_count][_addr][RandomGroup[_count][msg.sender].length].value;
 			}else{
-				_bounter.push(_addr)
+				_bounter[counter++]=_addr;
 			}
 		}
 
@@ -154,7 +204,7 @@ contract RandomFaas {
 
 		for(uint cnt=0;cnt<_bounter.length;cnt++){
 			_addr = _bounter[cnt];
-			_addr.send(_payback);
+			_addr.transfer(_payback);
 		}
 	}
 	
@@ -177,7 +227,7 @@ contract RandomFaas {
 	}
 	
 	modifier only(address _addr) { 
-		require(msg.sender = _addr)
+		require(msg.sender = _addr);
 		_; 
 	}
 
