e7f131ec5cfd7668c99ed286d38bedbe28a23895
lizhengxiang <info@genaro.network>
2019-01-28 17:07:02 +0800

    minor change to recover

--- RandomFaas.sol
+++ RandomFaas.sol
@@ -118,7 +118,7 @@ contract RandomFaas {
 	{  
 		Req storage latestInfo = RandomGroup[_count][msg.sender][RandomGroup[_count][msg.sender].length];
 		require(latestInfo.signature[0] == 0);
-        require(ecrecovery(_message,_signature)==msg.sender);
+        require(recover(_message,_signature)==msg.sender);
 	
  	    updateValue(RandomGroup[_count][msg.sender],0,_message,0);	
 		GroupOption[_count].NoQuitFlag = true;
@@ -126,40 +126,65 @@ contract RandomFaas {
 
 	}
 	
-//https://blog.csdn.net/code_segment/article/details/79952722
-//for the signature recovery function with minor changes
-
-function ecrecovery(bytes32 hash, bytes memory sig) public returns (address) {
+//from open-zepplin EC-Recovery
+  /**
+   * @dev Recover signer address from a message by using their signature
+   * @param _hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+   * @param _sig bytes signature, the signature is generated using web3.eth.sign()
+   */
+  function recover(bytes32 _hash, bytes memory _sig)
+    internal
+    pure
+    returns (address)
+  {
     bytes32 r;
     bytes32 s;
     uint8 v;
 
-    if (sig.length != 65) {
-      return address(0);
+    // Check the signature length
+    if (_sig.length != 65) {
+      return (address(0));
     }
 
+    // Divide the signature in r, s and v variables
+    // ecrecover takes the signature parameters, and the only way to get them
+    // currently is to use assembly.
+    // solium-disable-next-line security/no-inline-assembly
     assembly {
-      r := mload(add(sig, 32))
-      s := mload(add(sig, 64))
-      v := and(mload(add(sig, 65)), 255)
+      r := mload(add(_sig, 32))
+      s := mload(add(_sig, 64))
+      v := byte(0, mload(add(_sig, 96)))
     }
 
-    // https://github.com/ethereum/go-ethereum/issues/2053
+    // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
     if (v < 27) {
       v += 27;
     }
 
+    // If the version is correct return the signer address
     if (v != 27 && v != 28) {
-      return address(0);
+      return (address(0));
+    } else {
+      // solium-disable-next-line arg-overflow
+      return ecrecover(_hash, v, r, s);
     }
+  }
 
-    /* prefix might be needed for geth only
-     * https://github.com/ethereum/go-ethereum/issues/3731
-     */
-    // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-    // hash = sha3(prefix, hash);
-
-    return ecrecover(hash, v, r, s);
+  /**
+   * toEthSignedMessageHash
+   * @dev prefix a bytes32 value with "\x19Ethereum Signed Message:"
+   * and hash the result
+   */
+  function toEthSignedMessageHash(bytes32 _hash)
+    internal
+    pure
+    returns (bytes32)
+  {
+    // 32 is the length in bytes of hash,
+    // enforced by the type signature above
+    return keccak256(
+      abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)
+    );
   }
 
  	function closeProcess (uint _count) public  returns (bytes32 _result)
